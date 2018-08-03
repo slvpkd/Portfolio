63bc33677d6a6ffbc116ade6704abf22cf92c7af
Sefton <slvpkd@hotmail.com>
2018-08-03 19:06:05 +0100

    Portfolio Website
    First commit of portfolio site

---
+++ js/portfolio.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+
+  $('#characterLeft').text('140 characters left');
+  $('#message').keydown(function () {
+    var max = 140;
+    var len = $(this).val().length;
+    if (len >= max) {
+      $('#characterLeft').text('You have reached the limit');
+      $('#characterLeft').addClass('red');
+      $('#btnSubmit').addClass('disabled');
+    } else {
+      var ch = max - len;
+      $('#characterLeft').text(ch + ' characters left');
+      $('#btnSubmit').removeClass('disabled');
+      $('#characterLeft').removeClass('red');
+    }
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 1,
+        nav: true
+      },
+      1000: {
+        items: 1,
+        nav: true,
+        loop: true
+      }
+    }
+  })
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
+});
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function () {
+    var number = $(this).find('.number').text();
+    return parseInt(number, 10) > 50;
+  },
+  // show if name ends with -ium
+  ium: function () {
+    var name = $(this).find('.name').text();
+    return name.match(/ium$/);
+  }
+};
+// bind filter button click
+$('.filters-button-group').on('click', 'button', function () {
+  var filterValue = $(this).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[filterValue] || filterValue;
+  $grid.isotope({
+    filter: filterValue
+  });
+});
+// change is-checked class on buttons
+$('.button-group').each(function (i, buttonGroup) {
+  var $buttonGroup = $(buttonGroup);
+  $buttonGroup.on('click', 'button', function () {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $(this).addClass('is-checked');
+  });
+});
+
+"use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(this).toggleClass("active");
+  });
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('#sidebar-wrapper .js-scroll-trigger').click(function () {
+    $("#sidebar-wrapper").removeClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+});
